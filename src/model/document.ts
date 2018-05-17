66f742712d5ca042532565df411e9bfeec153359
Anthony Bushinger <abushinger@metlife.com>
2018-05-17 10:30:26 -0400

    model tests

--- src/model/document.ts
+++ src/model/document.ts
@@ -15,36 +15,32 @@ function incrVersion(version: string) {
     return ([ major, minor, revision + 1 ]).join('.');
 }
 
-export function create(documentType: string, userId: string = 'composer', head: any = {}, body: any = {}) : DocType {
+export function create(documentType: string, userId: string, head: any, body: any) : DocType {
     const ts = getTs();
-    const schema: DocSchema = {
-        version: START_VERSION,
-        type: documentType
-    };
 
-    const meta: DocMeta = {
-        version: START_VERSION,
-        createdOn: ts,
-        createdBy: userId,
-        updatedOn: ts,
-        updatedBy: userId       
-    }
-
-    const doc: DocType = {
+    return {
         id: uuid.v4(),
-        schema,
-        meta,
+        schema: {
+            version: START_VERSION,
+            type: documentType
+        },
+        meta: {
+            version: START_VERSION,
+            createdOn: ts,
+            createdBy: userId,
+            updatedOn: ts,
+            updatedBy: userId
+        },
         head,
         body
     }
-
-    return doc;
 }
-/*
-function update(model, userId = 'composer', head = {}, body = {}) {
+
+export function update(model: DocType, userId: string, head: any, body: any) : DocType {
+    const meta: any = model.meta || { version: '1.0.0' };
     return Object.assign({}, model, {
-        meta: Object.assign({}, model.meta || {}, {
-            version: incrVersion(model.meta.version),
+        meta: Object.assign({}, meta, {
+            version: incrVersion(meta.version),
             updatedOn: getTs(),
             updatedBy: userId
         }),
@@ -53,46 +49,42 @@ function update(model, userId = 'composer', head = {}, body = {}) {
     });
 }
 
-function patch(model, userId = 'composer', head = {}, body = {}) {
+export function patch(model: DocType, userId: string, head: any, body: any) : DocType {
     return update(model, userId, 
         Object.assign({}, model.head, head),
         Object.assign({}, model.body, body));
 }
 
-function extractHeader(model) {
-    return omit(model, 'body');
+export function extractHeader(model: DocType) : DocType {
+    return lodash.omit(model, 'body');
 }
 
-function _toLowerCase(text) {
+function _toLowerCase(text: string) {
     return ((text || '') + '').toLowerCase();
 }
 
-function _includes(c,v) {
+function _includes(c: string,v: string) {
     return _toLowerCase(c).includes(_toLowerCase(v));
 }
 
-const ops = {
-    eq: (c,v) => c === v,
-    neq: (c,v) => c != v,
-    lt: (c,v) => v < c,
-    lte: (c,v) => v <= c,
-    gt: (c,v) => v > c,
-    gte: (c,v) => v >= c,
-    isnull: c => c == null,
-    notnull: c => c != null,    
-    incl: _includes
+const ops = [
+    { op: 'eq', fn: (c: any,v: any) => c === v },
+    { op: 'neq', fn: (c: any,v: any) => c != v },
+    { op: 'lt', fn: (c: any,v: any) => v < c },
+    { op: 'lte', fn: (c: any,v: any) => v <= c },
+    { op: 'gt', fn: (c: any,v: any) => v > c },
+    { op: 'gte', fn: (c: any,v: any) => v >= c },
+    { op: 'isnull', fn: (c: any) => c == null },
+    { op: 'notnull', fn:  (c: any) => c != null },    
+    { op: 'incl', fn: _includes }
+];
+
+function getOp(op: string) : any {
+    const found = ops.find(o => o.op === op);
+    return (found) ? found.fn : getOp('eq');
 }
 
-function filterBy(list, path, value, op = 'eq') {
+export function filterBy(list: DocType[], path: string, value: any, op: string = 'eq') : DocType[] {
     return list.filter(item => 
-        ops[op](get(item, path, null), value));
+        getOp(op)(lodash.get(item, path, null), value));
 }
-
-module.exports = {
-    create,
-    update,
-    patch,
-    extractHeader,
-    filterBy
-}
-*/
--- test/src/model/document.spec.ts
+++ test/src/model/document.spec.ts
@@ -1,23 +1,65 @@
+import { describe, it } from 'mocha';
 import assert from 'assert';
-import { create } from '../../../src/model/document';
+import { create, update, patch, extractHeader, filterBy } from '../../../src/model/document';
 
 describe('model - document', function() {
   describe('create', function() {
     it('should create model (no params)', function() {
-      const result = create('my model');      
+      const result = create('my model','userA',{},{});      
       assert(result);
     });
   });
-/*
+
   describe('update', function() {
-    it('should update model', function() {
-      const testModel = model.create('testmodel');
-      const result = model.update(testModel, 'testUser', { updated: true }, { updated: true });      
-      //console.log(result);
-      assert(result.meta.version === '1.0.1');
-      assert(result.head.updated);
-      assert(result.body.updated);      
+    describe('normal', function() {
+      it('should update model', function() {
+        const testModel = create('testmodel','userA',{},{});
+        const result = update(testModel, 'testUser', { updated: true }, { updated: true });      
+        //console.dir(result, { colors: true });
+        assert(result);
+        assert(result.meta.version === '1.0.1');
+      });
+    });
+
+    describe('missing meta section', function() {
+      it('should update model', function() {
+        const testModel = create('testmodel','userA',{},{});
+        testModel.meta = null;
+        const result = update(testModel, 'testUser', { updated: true }, { updated: true });      
+        //console.dir(result, { colors: true });
+        assert(result);
+        assert(result.meta.version === '1.0.1');
+      });
     });
   });
-*/
+
+  describe('patch', function() {
+    it('should update model', function() {
+      const testModel = create('testmodel','userA',{},{});
+      const result = patch(testModel, 'testUser', { updated: true }, { updated: true });      
+      //console.dir(result, { colors: true });
+      assert(result);
+      assert(result.meta.version === '1.0.1');
+    });
+  });
+
+  describe('extractHeader', function() {
+    it('should remove body', function() {
+      const testModel = create('testmodel','userA',{},{});
+      const result = extractHeader(testModel);      
+      //console.dir(result, { colors: true });
+      assert(result);
+      assert(result.body === undefined);
+    });
+  });
+
+  describe('filterBy', function() {
+    it('should remove body', function() {
+      const testModel1 = create('testmodel','userA',{},{});
+      const testModel2 = create('testmodel','userA',{},{});
+      const result = filterBy([testModel1,testModel2], 'meta.createdBy', 'userA');      
+      //console.dir(result, { colors: true });
+      assert(result.length == 2);
+    });
+  });
 });
