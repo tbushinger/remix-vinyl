25e11c758139cc3e65bcd509dadff9adeee4342f
tbushinger <tbushinger@gmail.com>
2018-07-07 21:34:57 -0400

    Update test.sh

--- test/integration/test.sh
+++ test/integration/test.sh
@@ -4,6 +4,8 @@ DOC_USER=myDocUser
 FIXTURES_DIR=/scripts/fixtures
 URL=http://remix:3000/documents
 
+sleep 10s
+
 curl -d "@${FIXTURES_DIR}/post.json" \
   -H "userid: ${DOC_USER}" \
   -H "Content-Type: application/json" \
